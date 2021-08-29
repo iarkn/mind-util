56ffc7c626915fb32bcf0472a6b068feba7f0069
iarkn <iarknrh@gmail.com>
2021-08-29 09:14:30 +0700

    Rewrite utilities menu layout

--- src/main/java/iarkn/mutl/dialogs/UtilitiesDialog.java
+++ src/main/java/iarkn/mutl/dialogs/UtilitiesDialog.java
@@ -21,45 +21,29 @@ public class UtilitiesDialog extends BaseDialog {
         tools = new Table(Tex.button);
         config = new Table(Tex.button);
 
-        shown(() -> rebuild(true));
+        shown(this::rebuild);
         addCloseButton();
     }
 
-    public void rebuild(boolean showTools) {
+    public void rebuild() {
         cont.clearChildren();
         tools.clearChildren();
-        config.clearChildren();
 
-        if (showTools) {
-            tools.defaults().size(300f, 60f);
+        tools.defaults().size(300f, 60f);
 
-            // options.button("@mutl.option.config", Styles.cleart, Index.display::show);
-            tools.button("@mutl.tool.world", Styles.cleart, Index.world::show).disabled(!Vars.state.isGame()).row();
-            tools.button("@mutl.tool.unitspawner", Styles.cleart, Index.spawner::show).disabled(!Vars.state.isGame()).row();
-            // options.button("@mutl.option.modding", Styles.cleart, Index.modding::show);
-        } else {
-            config.defaults().size(300f, 60f);
+        tools.button("@mutl.tool.world", Styles.cleart, Index.world::show).disabled(!Vars.state.isGame()).row();
+        tools.button("@mutl.tool.unitspawner", Styles.cleart, Index.spawner::show).disabled(!Vars.state.isGame()).row();
 
-            // TODO
-        }
+        // TODO Finish configuration table
 
         cont.table(t -> {
-            t.table(Tex.button, t2 -> {
-                t2.table(t3 -> {
-                    t3.left();
-                    t3.defaults().size(48f);
+            t.table(Tex.button, t2 -> t2.add("@mutl.header.tools")).grow();
+            t.button(Icon.settings, () -> {}).size(60f).padLeft(6f);
+        }).size(324f, 60f).padBottom(6f);
 
-                    t3.button(Icon.list, Styles.cleari, () -> rebuild(true));
-                    t3.button(Icon.settings, Styles.cleari, () -> rebuild(false));
-                }).grow();
+        cont.row();
 
-                t2.add(showTools ? "@mutl.tools" : "@mutl.config").grow();
-            }).grow().padBottom(6f);
-
-            t.row();
-
-            t.add(showTools ? tools : config);
-        }).width(324f);
+        cont.add(tools);
     }
 
     public void rebind(String key) {
